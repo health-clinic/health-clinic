a91f0816ebd891c7044028aba9daaca3b6bc12fa
William Trevisan <williamtrev@hotmail.com>
2025-07-05 14:51:10 -0300

    chore(infra): adjust authorization retrieve

--- infra/pulumi/modules/ecr.ts
+++ infra/pulumi/modules/ecr.ts
@@ -1,23 +1,30 @@
 import * as awsx from '@pulumi/awsx/classic';
 import * as aws from '@pulumi/aws';
 import * as docker from '@pulumi/docker-build';
+import * as pulumi from '@pulumi/pulumi';
 
-export const ecr = new awsx.ecr.Repository('health-clinic-ecr-repo');
+export const ecr = new awsx.ecr.Repository('health-clinic-ecr');
 
-const { username, password } = aws.ecr.getAuthorizationTokenOutput({
+const authorization = aws.ecr.getAuthorizationTokenOutput({
   registryId: ecr.repository.registryId,
 });
 
-export const image = new docker.Image('health-clinic-app-image', {
-  context: './../../../',
+export const image = new docker.Image('health-clinic-image', {
+  context: { location: './../../../' },
   platforms: ['linux/amd64'],
   push: true,
   registries: [
-    {
-      address: ecr.repository.repositoryUrl,
-      username,
-      password,
-    },
+    pulumi.all([ecr.repository.repositoryUrl, authorization]).apply(([url, token]) => {
+      const [username, password] = Buffer.from(token.authorizationToken, 'base64')
+        .toString('utf8')
+        .split(':');
+
+      return {
+        address: url,
+        username,
+        password,
+      };
+    }),
   ],
   tags: [ecr.repository.repositoryUrl.apply((url: any): string => `${url}:latest`)],
 });
